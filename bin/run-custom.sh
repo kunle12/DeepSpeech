35bdd69d68ccd6d5ebc56657ee59d5f0a1e3dd47
Xun Wang <wang.xun@gmail.com>
2019-05-29 10:48:10 +1000

    minor adjustment.

--- bin/run-custom.sh
+++ bin/run-custom.sh
@@ -10,14 +10,17 @@ python3 -u DeepSpeech.py \
   --dev_files dev-random.csv \
   --test_files test-random.csv \
   --train_batch_size 24 \
-  --dev_batch_size 32 \
-  --test_batch_size 32 \
+  --dev_batch_size 12 \
+  --test_batch_size 12 \
   --n_hidden 2048 \
   --learning_rate 0.0001 \
   --dropout_rate 0.15 \
   --lm_binary_path /srv/ml_datasets/speech_data/language_corpora/lm.binary \
   --lm_trie_path /srv/ml_datasets/speech_data/language_corpora/trie \
   --epochs 50 \
+  --early_stop false \
   --feature_cache /srv/ml_datasets/tmp/tfcache \
   --checkpoint_dir checkpoint \
+  --summary_dir summary \
+  --export_dir exports \
   "$@"
